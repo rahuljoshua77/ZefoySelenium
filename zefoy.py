7ae7c37193931e96a36f40b63fb81977d9faf0ab
Rahul Joshua Damanik <73378179+rahuljoshua77@users.noreply.github.com>
2021-09-17 19:57:23 +0700

    Update zefoy.py

--- zefoy.py
+++ zefoy.py
@@ -11,12 +11,13 @@ from selenium.webdriver.common.action_chains import ActionChains
 import random
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
+from multiprocessing import Pool
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import os
+import os,re
 from python_anticaptcha import AnticaptchaClient, ImageToTextTask
 cwd = os.getcwd()
 
@@ -51,9 +52,43 @@ def open_browser():
     browser = webdriver.Chrome(options=opts, desired_capabilities=dc)
     menu()
 
-def menu():
+def reload():
     browser.get("https://zefoy.com")
-    print(f"[*] Open Zefoy")
+    print(f"[*] Reload!")
+    try:
+        img_captcha = wait(browser,10).until(EC.presence_of_element_located((By.XPATH, '//img[@class="img-thumbnail card-img-top"]')))
+        sleep(0.5)
+        captcha_image = img_captcha.screenshot("captcha_image.png")
+        print(f"[*] Solving Captcha")
+        captcha_text = get_captcha_text(captcha_image)
+        print(f"[*] Captcha Solved: {captcha_text} ")
+        captcha_fill = wait(browser,10).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Enter the word"]')))
+        captcha_fill.send_keys(captcha_text) 
+        captcha_fill.send_keys(Keys.ENTER)
+        
+        #tittle service
+    except:
+        pass
+    sleep(2)
+    wait(browser,15).until(EC.presence_of_all_elements_located((By.XPATH, '//button[contains(@class,"btn btn-primary rounded-0 menu")]')))
+    buttons = wait(browser,15).until(EC.presence_of_all_elements_located((By.XPATH, '//button[contains(@class,"btn btn-primary rounded-0 menu")]')))
+    input_videos = wait(browser,15).until(EC.presence_of_all_elements_located((By.XPATH, '(//input[@placeholder="Enter Video URL"])')))
+  
+    try:
+        click = buttons[choice].click()
+        input_videos[choice].send_keys(vid_obj)
+    except Exception as e:
+        print(e)
+def menu():
+    global buttons
+    global choice
+    global input_videos
+    global click
+    global vid_obj
+
+
+    browser.get("https://zefoy.com")
+    print(f"[*] Zevoy Tiktok Bot Automation\n[*] Author: RJD")
     #check captcha
     sleep(3)
     try:
@@ -87,31 +122,41 @@ def menu():
     input_videos[choice].send_keys(vid_obj)
     for i in range(0, loop):
         input_videos[choice].send_keys(Keys.ENTER)
+        
         print(f"[*] Submit {j}")
         j = j+1
         sleep(1)
         try:
-            wait(browser,15).until(EC.presence_of_element_located((By.XPATH,'//button[@class,"abjdt wbutton oalenus btn srthjv btn-dark jmw rounded-0 egbv font-weight-bold oxip p-2 vcndajshxpbw"]'))).click()
+            wait(browser,15).until(EC.presence_of_element_located((By.XPATH,'//button[contains(@class,"wbutton")]'))).click()
         except:
             pass
         try:
             notif = wait(browser,1).until(EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'Session Expired. Please Re Login! ')]")))
-            print(f"[*] Session Expired, Back to main menu!")
-            break
+            print(f"[*] Session Expired, wait re-log!")
+
+            reload()
+            browser.save_screenshot("ZEFOY-1.png")
+            input_videos[choice].send_keys(Keys.ENTER)
         except:
             pass
         sleep(0.5)
-        #browser.save_screenshot("ZEFOY.png")
-        print(f"[*] Please wait 2 minutes")
-        sleep(120)
+        # browser.save_screenshot("ZEFOY.png")
+       
+        get_delay = wait(browser,35).until(EC.presence_of_element_located((By.XPATH, "//h4[contains(text(),'Please wait')]"))).text
+        clear_time = re.findall(r'\b\d+\b', str(get_delay))
+        print(f"[*] Submit Done, Please wait {clear_time[0]} minutes {clear_time[1]}s")
+        minutes = int(clear_time[0])*60
+        second = clear_time[1]
+        delay = int(minutes)+int(second)
+       
+        sleep(delay)
+
     input_choice = input("[*] Back to Menu? Y/n (n = end/close)")
     input_choice = input_choice.lower()
     if input_choice == "y":
         menu()
     else:
         browser.quit()
-    
-
 
     
 open_browser()
